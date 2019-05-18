935540f023dc657531a18d9a56b0cc2773f97716
Gilvan <camisetaecia@live.com>
2019-05-18 10:54:20 -0300

    Passing a Department object to DepartmentForm view

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,6 +1,7 @@
 package gui;
 
 import java.net.URL;
+import java.nio.channels.IllegalSelectorException;
 import java.util.ResourceBundle;
 
 import gui.util.Constraints;
@@ -9,9 +10,12 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.entities.Department;
 
 public class DepartmentFormController implements Initializable {
-
+	
+	private Department entity;
+	
 	@FXML
 	private TextField txtId;
 	
@@ -26,6 +30,10 @@ public class DepartmentFormController implements Initializable {
 	
 	@FXML
 	private Button btCancel;
+	
+	public void setDepartment(Department entity) {
+		this.entity = entity;
+	}
 	
 	@FXML
 	private void onBtSaveAction() {
@@ -46,6 +54,14 @@ public class DepartmentFormController implements Initializable {
 	private void initializeNodes() {
 		Constraints.setTextFieldInteger(txtId);
 		Constraints.setTextFieldMaxLength(txtName, 33);		
+	}
+	
+	public void updateFormData() {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was nulll"); 
+		}
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
 	}
 	
 }
--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -46,7 +46,8 @@ public class DepartmentListController implements Initializable {
 	
 	public void onBtNewAction(ActionEvent event) {
 		Stage parentStage = Utils.currentStage(event);
-		createDialoForm("/gui/DepartmentForm.fxml", parentStage);
+		Department obj = new Department();
+		createDialoForm(obj,"/gui/DepartmentForm.fxml", parentStage);
 	}
 	
 	public void setDepartmentService(DepartmentService service) {
@@ -78,10 +79,15 @@ public class DepartmentListController implements Initializable {
 		
 	}//fim do updateTableView
 	
-	private void createDialoForm(String absoluteName, Stage parentStage) {
+	private void createDialoForm(Department obj,String absoluteName, Stage parentStage) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			Pane pane = loader.load();
+			
+			DepartmentFormController controller = loader.getController();
+			controller.setDepartment(obj);
+			controller.updateFormData();
+			
 			Stage dialogStage = new Stage();
 			dialogStage.setTitle("Enter Department Data");
 			dialogStage.setScene(new Scene(pane));
